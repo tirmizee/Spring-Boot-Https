6222a8f88f59f8baa30b2597f0a6207fd58cd84f
Pratya Yeekhaday <pratya.y@gdl-technology.com>
2020-03-21 17:19:35 +0700

    update readme.md

---
+++ Spring-Boot-SSL-Client-TrustStore/readme.md
@@ -0,0 +1,17 @@
+### Way 1
+ 
+	static {
+		ClassLoader classLoader = SpringBootSslTrustStoreApplication.class.getClassLoader();
+		File file = new File(classLoader.getResource("client-truststore.jks").getFile());
+		String absolutePath = file.getAbsolutePath();
+		System.setProperty("javax.net.ssl.trustStore",absolutePath);
+		System.setProperty("javax.net.ssl.trustStorePassword", "clintstorepass");
+		System.setProperty("javax.net.ssl.trustStoreType", "JKS");
+	}
+	
+### Way 2
+
+	-Djavax.net.ssl.trustStore=C:\Users\Lenovo-User\eclipse-workspace\Spring-Boot-Https\Spring-Boot-SSL-Client-TrustStore\target\classes\client-truststore.jks
+	-Djavax.net.ssl.trustStorePassword=clintstorepass
+	-Djavax.net.ssl.trustStoreType=JKS
+	-Djavax.net.debug=all
--- Spring-Boot-SSL-Client-TrustStore/src/main/java/com/tirmizee/SpringBootSslTrustStoreApplication.java
+++ Spring-Boot-SSL-Client-TrustStore/src/main/java/com/tirmizee/SpringBootSslTrustStoreApplication.java
@@ -9,14 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootSslTrustStoreApplication implements CommandLineRunner {
 
-	static {
-		ClassLoader classLoader = SpringBootSslTrustStoreApplication.class.getClassLoader();
-		File file = new File(classLoader.getResource("client-truststore.jks").getFile());
-		String absolutePath = file.getAbsolutePath();
-		System.setProperty("javax.net.ssl.trustStore",absolutePath);
-		System.setProperty("javax.net.ssl.trustStorePassword", "clintstorepass");
-		System.setProperty("javax.net.ssl.trustStoreType", "JKS");
-	}
+//	static {
+//		ClassLoader classLoader = SpringBootSslTrustStoreApplication.class.getClassLoader();
+//		File file = new File(classLoader.getResource("client-truststore.jks").getFile());
+//		String absolutePath = file.getAbsolutePath();
+//		System.setProperty("javax.net.debug","all");
+//		System.setProperty("javax.net.ssl.trustStore",absolutePath);
+//		System.setProperty("javax.net.ssl.trustStorePassword", "clintstorepass");
+//		System.setProperty("javax.net.ssl.trustStoreType", "JKS");
+//	}
 	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootSslTrustStoreApplication.class, args);
